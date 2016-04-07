4d78336903e980876fe5cb6db8463e1b4046ac4c
Benoit Sigoure <tsunanet@gmail.com>
2016-04-07 00:28:30 -0700

    Specify the license to GPLv3.

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
+# Copyright (C) 2010  Benoit Sigoure
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 CC = gcc
 CFLAGS = -march=native -O3 -mno-avx -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -std=c99 \
          -W -Wall -Werror
--- cpubench.sh
+++ cpubench.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Copyright (C) 2010  Benoit Sigoure
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 fgrep 'model name' /proc/cpuinfo | sort -u | xargs
 pgrep vmware >/dev/null && echo Running under VMware
 ncpus=`sort -u /sys/devices/system/cpu/cpu*/topology/physical_package_id | wc -l`
